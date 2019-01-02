c13f9e6c8bb44af76fc45afaa9be8a422bca7b0f
Ali Itani <aliitani0@gmail.com>
2019-01-02 12:45:32 -0500

    resume added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,6 @@
 
 // function for getting links sent to the buttons.
 function openLinks(url) {
-    let links = ['https://github.com/aliitani', 'https://www.linkedin.com/in/aliitani0/', 'https://angel.co/aliitani', '', 'https://medium.com/@aliitani0', 'https://www.instagram.com/aliitani/', 'https://twitter.com/antecedentsoft'];
+    let links = ['https://github.com/aliitani', 'https://www.linkedin.com/in/aliitani0/', 'https://angel.co/aliitani', '', 'https://medium.com/@aliitani0', 'https://www.instagram.com/aliitani/', 'https://twitter.com/antecedentsoft', './assets/resume/Ali-Resume.pdf'];
     window.open(links[url], '_blank').focus();
 }
