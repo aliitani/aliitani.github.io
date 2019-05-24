dbed489b2d80fe5c9c1764032c6f891fb1716fe6
Ali Itani <aliitani0@gmail.com>
2019-05-23 23:34:26 -0400

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,13 @@
 
 // function for getting links sent to the buttons.
 function openLinks(url) {
-    let links = ['https://github.com/aliitani', 'https://www.linkedin.com/in/aliitani0/', 'https://angel.co/aliitani', '', 'https://medium.com/@aliitani0', 'https://www.instagram.com/aliitani/', 'https://twitter.com/atomsoftworks'];
+    let links = ['https://github.com/aliitani', 'https://www.linkedin.com/in/aliitani0/', 
+    'https://angel.co/aliitani', '', 'https://medium.com/@aliitani0', 
+    'https://www.instagram.com/aliitani/', 'https://twitter.com/atomsoftworks'];
+    window.open(links[url], '_blank').focus();
+}
+
+function openProjects(url){
+    let links = ['https://overwatch-hero-database.com'];
     window.open(links[url], '_blank').focus();
 }
